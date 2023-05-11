d56667f833729689be8ab7db305b75309520139d
OmkarSatav <omkar7v2@gmail.com>
2023-05-11 20:55:58 +0530

    Changes Made

--- attendance-system/data/script4.js
+++ attendance-system/data/script4.js
@@ -4,46 +4,52 @@ const loginBox = document.getElementById("login-box");
 const signupBox = document.getElementById("signup-box");
 
 signupBox.classList.add("show");
+let updatedJsonData;
+async function goTo() {
+    await fetch("attendence.json")
+        .then(response => response.json())
+        .then(jsonData => {
+            // Modify the JSON data in memory
+            jsonData["TE1"]["DSBDA"]["19-04-2023"].push("TC176");
 
-function goTo() {
-  fetch("attendence.json")
-    .then(response => response.json())
-    .then(jsonData => {
-      // Modify the JSON data in memory
-      jsonData["TE1"]["DSBDA"]["19-04-2023"].push("TC176");
+            // Write the modified JSON data back to the file
+            console.log(jsonData);
+            updatedJsonData = JSON.stringify(jsonData);
 
-      // Write the modified JSON data back to the file
-      const updatedJsonData = JSON.stringify(jsonData);
-      fetch("attendence.json", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: updatedJsonData
-      })
-        .then(() => console.log("Data added to JSON file"))
+            fetch("attendence.json", {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: updatedJsonData
+            })
+                .then(response => response.json())
+                .then(() => console.log("Data added to JSON file"))
+                .catch(error => console.error(error));
+
+        })
         .catch(error => console.error(error));
-    })
-    .catch(error => console.error(error));
 
 
 
-
-  // window.location.replace("http://192.168.4.1/ty?r=" + btoa(document.getElementById("nrn").value));
+    // window.location.replace("http://192.168.4.1/ty?r=" + btoa(document.getElementById("nrn").value));
 }
 
+
+
+
 loginBtn.addEventListener("click", function () {
-  loginBox.classList.add("show");
-  signupBox.classList.remove("show");
-  console.log(ip);
+    loginBox.classList.add("show");
+    signupBox.classList.remove("show");
+    console.log(ip);
 });
 
 signupBtn.addEventListener("click", function () {
-  signupBox.classList.add("show");
-  loginBox.classList.remove("show");
+    signupBox.classList.add("show");
+    loginBox.classList.remove("show");
 
 });
 
 function teacherLogin() {
-  window.location.replace("http://192.168.4.1/admin");
+    window.location.replace("http://192.168.4.1/admin");
 }
